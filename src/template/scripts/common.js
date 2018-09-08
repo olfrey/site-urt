e205ffc532686ede26398fb02279cb6eb47cc5c6
e5ash <e5ash.bro@gmail.com>
2018-09-08 04:27:45 +0300

    100%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,67 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+
+	$('.reviews__list').slick({
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		dots: true,
+		responsive: [{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}}, {
+			breakpoint: 620,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: false
+			}
+		}]
+	});
+
+	$('.steps__item').hover(function() {
+		if (!$(this).hasClass('steps__item_active')) {
+			$('.steps__item').removeClass('steps__item_active');
+			$(this).addClass('steps__item_active');
+		}
+	});
+
+
+	// $('.popup__images').each(function(index, el) {
+		
+	// });
+
+	$('.catalog__btn').click(function(event) {
+		var catalogItem = $(this).parents('.catalog__item'),
+				popupImage = catalogItem.find('.popup__images-img'),
+				popupImages = catalogItem.find('.popup__images-imgs');
+
+		popupImage.slick({
+			asNavFor: popupImages,
+			arrows: false
+		});
+
+		popupImages.slick({
+			asNavFor: popupImage,
+			arrows: false,
+			focusOnSelect: true,
+			slidesToShow: 3
+		});
+	});
+
+	$("a[href^='#']").click(function(){
+	    var _href = $(this).attr("href");
+	    $("html, body").animate({scrollTop: $(_href).offset().top+"px"}, 1500);
+	    return false;
+	});
+	
+	var humburger = $('.hum'),
+			navigation = $('.panel__nav');
+
+	humburger.click(function(event) {
+		$(this).toggleClass('hum_toggle');
+		navigation.toggleClass('panel__nav_toggle');
+	});
 });
